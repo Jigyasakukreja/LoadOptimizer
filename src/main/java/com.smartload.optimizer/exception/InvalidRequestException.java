bae2c3399df13edbc473897526d2853c7fe321da
Jigyasa <85480403+Jigyasakukreja@users.noreply.github.com>
2026-01-14 13:32:17 +0530

    Delete src/main/java/com.smartload.optimizer directory

--- src/main/java/com.smartload.optimizer/exception/InvalidRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.smartload.optimizer.exception;
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
